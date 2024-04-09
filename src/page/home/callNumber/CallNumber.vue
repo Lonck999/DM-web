07b014220f90cb3c7e8d7a0803f7583ede53aaff
Lonck999 <joe22053814@gmail.com>
2024-04-09 17:49:54 +0800

    updata callNumber content

--- src/page/home/callNumber/CallNumber.vue
+++ src/page/home/callNumber/CallNumber.vue
@@ -2,62 +2,92 @@
     <div class="call">
         <h1>{{ title }}</h1>
         <div class="number">
-            <div class="number-dark">
-                <div class="number-room">
-                    <p>一</p>
-                    <p>診</p>
+            <div class="number-block" v-for="call in callNumber" :key="call.id">
+                <div class="number-dark" v-if="call.dark">
+                    <div class="number-room">
+                        <p>{{ call.roomNameTop }}</p>
+                        <p>{{ call.roomNameBottom }}</p>
+                    </div>
+                    <div class="number-clinic">
+                        <p>{{ call.clinic }}<span class="green">{{ call.clinicName }}</span></p>
+                        <p>{{ call.now }}</p>
+                        <p class="green now">{{ call.nowNumber }}</p>
+                    </div>
+                    <div class="number-clinic">
+                        <p>{{ call.doctor }}<span class="green">{{ call.doctorName }}</span></p>
+                        <p>{{ call.waiting }}</p>
+                        <p class="now">{{ call.waitingNumber }}</p>
+                    </div>
                 </div>
-                <div class="number-clinic">
-                    <p>科別 - <span class="green">外科</span></p>
-                    <p>看診號碼</p>
-                    <p class="green now">1</p>
+                <div class="number-light" v-else>
+                    <div class="number-room">
+                        <p>{{ call.roomNameTop }}</p>
+                        <p>{{ call.roomNameBottom }}</p>
+                    </div>
+                    <div class="number-clinic">
+                        <p>{{ call.clinic }}<span class="green">{{ call.clinicName }}</span></p>
+                        <p>{{ call.now }}</p>
+                        <p class="green now">{{ call.nowNumber }}</p>
+                    </div>
+                    <div class="number-clinic">
+                        <p>{{ call.doctor }}<span class="green">{{ call.doctorName }}</span></p>
+                        <p>{{ call.waiting }}</p>
+                        <p class="now">{{ call.waitingNumber }}</p>
+                    </div>
                 </div>
-                <div class="number-clinic">
-                    <p>醫師 - <span class="green">李明志</span></p>
-                    <p>候診人數</p>
-                    <p class="now">3</p>
-                </div>
-            </div>
-            <div class="number-light">
-                <div class="number-room">
-                        <p>二</p>
-                        <p>診</p>
-                    </div>
-                    <div class="number-clinic">
-                        <p>科別 - <span class="green">外科</span></p>
-                        <p>看診號碼</p>
-                        <p class="green now">1</p>
-                    </div>
-                    <div class="number-clinic">
-                        <p>醫師 - <span class="green">李明志</span></p>
-                        <p>候診人數</p>
-                        <p class="now">3</p>
-                    </div>
-            </div>
-            <div class="number-dark">
-                 <div class="number-room">
-                        <p>三</p>
-                        <p>診</p>
-                    </div>
-                    <div class="number-clinic">
-                        <p>科別 - <span class="green">外科</span></p>
-                        <p>看診號碼</p>
-                        <p class="green now">1</p>
-                    </div>
-                    <div class="number-clinic">
-                        <p>醫師 - <span class="green">李明志</span></p>
-                        <p>候診人數</p>
-                        <p class="now">3</p>
-                    </div>
             </div>
         </div>
     </div>
 </template>
-    
 <script setup lang='ts'>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 const title = ref('叫號進度');
+
+const callNumber = reactive([
+    {
+        id: 1,
+        roomNameTop: '一',
+        roomNameBottom: '診',
+        clinic: '科別 - ',
+        clinicName: '外科',
+        now: '看診號碼',
+        nowNumber: 1,
+        doctor: '醫師 - ',
+        doctorName: '李明志',
+        waiting: '候診人數',
+        waitingNumber: 3,
+        dark: true,
+    },
+    {
+        id: 2,
+        roomNameTop: '二',
+        roomNameBottom: '診',
+        clinic: '科別 - ',
+        clinicName: '外科',
+        now: '看診號碼',
+        nowNumber: 1,
+        doctor: '醫師 - ',
+        doctorName: '李明志',
+        waiting: '候診人數',
+        waitingNumber: 3,
+        dark: false,
+    },
+    {
+        id: 3,
+        roomNameTop: '三',
+        roomNameBottom: '診',
+        clinic: '科別 - ',
+        clinicName: '外科',
+        now: '看診號碼',
+        nowNumber: 1,
+        doctor: '醫師 - ',
+        doctorName: '李明志',
+        waiting: '候診人數',
+        waitingNumber: 3,
+        dark: true,
+    }
+]);
 
 </script>
     
@@ -82,26 +112,30 @@ const title = ref('叫號進度');
     .number {
         display: flex;
 
-        .number-dark,
-        .number-light {
+        .number-block {
             flex: 1;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            gap: 0 9%;
-            text-align: center;
-            padding: 3.2% 0;
 
-            .number-room {
-                background: #2bc0bc;
-                border-radius: 5px;
-                color: #f5f5f5;
-                font-size: 1.8cqw;
-                font-weight: 700;
-                padding: 8.5% 2%;
 
-                p:nth-child(1) {
-                    margin-bottom: 40%;
+            .number-dark,
+            .number-light {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                gap: 0 9%;
+                text-align: center;
+                padding: 9.5% 0;
+
+                .number-room {
+                    background: #2bc0bc;
+                    border-radius: 5px;
+                    color: #f5f5f5;
+                    font-size: 1.8cqw;
+                    font-weight: 700;
+                    padding: 8.5% 2%;
+
+                    p:nth-child(1) {
+                        margin-bottom: 40%;
+                    }
                 }
             }
 
@@ -124,14 +158,16 @@ const title = ref('叫號進度');
                     margin: 2.8cqw 0 1cqw 0;
                 }
             }
-        }
 
-        .number-dark {
-            background: url('../../../assets/cardbg-1.png');
-        }
+            .number-dark {
+                background: url('../../../assets/cardbg-1.png');
+                background-size: cover;
+            }
 
-        .number-light {
-            background: url('../../../assets/cardbg-2.png');
+            .number-light {
+                background: url('../../../assets/cardbg-2.png');
+                background-size: cover;
+            }
         }
     }
 }
