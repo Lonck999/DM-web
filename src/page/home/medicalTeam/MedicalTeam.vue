49ac9627bb24f7c03e35995b49e5f4126564be92
Lonck999 <joe22053814@gmail.com>
2024-04-17 18:09:10 +0800

    updata  clinicTable

--- src/page/home/medicalTeam/MedicalTeam.vue
+++ src/page/home/medicalTeam/MedicalTeam.vue
@@ -1,11 +1,37 @@
 <template>
-        <div>醫療團隊</div>
+    <div class="team">
+        <h2>醫療團隊</h2>
+        <div>
+            <div>
+                <img src="" alt="">
+                <div>
+                    <p>醫生</p>
+                    <p>眼科</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
-    
+
 <script setup lang='ts'>
-    
+
 </script>
-    
+
 <style>
-    
+.team {
+    max-width: 1920px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 4.5% 0 0 0;
+    container-type: inline-size;
+
+    h2{
+        text-align: center;
+            font-size: 1.7cqw;
+                color: #00908c;
+                font-style: normal;
+                font-weight: 700;
+                letter-spacing: 0.05em;
+    }
+}
 </style>
